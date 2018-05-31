3f7a70adfa1c46c7c25b0fcd71b8af6dfb1379a0
erickalexander <erickcamacho1@gmail.com>
2018-05-31 19:52:52 -0400

    setup bootstrap layout and started fetching from Youtube API and adding data to the DOM

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let searchTerm = "dog"
+let fetchedVideos = fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyA33f7TdGW7pXSjQCPpiX-f4PNn-IGq73g&q=${searchTerm}&type=video`)
+  .then(resp => resp.json()).then(resp => {loadVideo(resp)})
+
+function loadVideo(resp){
+  vidResults.innerHTML += `<div class="well"><h3>${resp.etag}</h3> <button class="btn btn-success"> + </button></div>`
+
+
+}
+let reset = getElementById('reset')
+
+function resetVideo(){
+  console.log("RESETTING");
+  vidResults.innerHTML = ``
+
+}
+
+function searchVideo(resp){
+
+}
