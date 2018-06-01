bf9650c998033a3fed83bcb3f94c9724a1f7915d
erickalexander <erickcamacho1@gmail.com>
2018-05-31 22:50:11 -0400

    search bar working

--- js/index.js
+++ js/index.js
@@ -1,33 +1,34 @@
   document.getElementById('myForm').addEventListener('submit',fetchVideos)
 
   console.log("LOADINGG");
-  let searchTerm = "cat"
-  let fetching = fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyA33f7TdGW7pXSjQCPpiX-f4PNn-IGq73g&q=${searchTerm}&type=video`)
-    .then(resp => resp.json())
+
 
   function fetchVideos(e){
+    let searchTerm = document.getElementById('searchterm').value
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyA33f7TdGW7pXSjQCPpiX-f4PNn-IGq73g&q=${searchTerm}&type=video`)
+      .then(resp => resp.json()).then(resp => {searchVideo(resp)})
+
     e.preventDefault()
     console.log("FETCHING VIDS OF", searchTerm);
 
-    fetching.then(resp => {searchVideo(resp)})
   }
 
-  function resetNow(){
-    vidResults.innerHTML = ``
-  }
 
   function searchVideo(resp){
+    vidResults.innerHTML = ``
+
     console.log('res',resp);
     resp.items.map(v => vidResults.innerHTML += `<div class="col-sm-4"> <div class="card" style="width: 18rem;"><img class="card-img-top" src="${v.snippet.thumbnails.default.url}">  <div class="card-body">
         <h5 class="card-title">${v.snippet.title}</h5>
-        <p class="card-text">${v.snippet.title}</p>
-        <a href="#" class="btn btn-primary">Save</a>
+        <h6 class="card-text">${v.snippet.description}</h6>
+        <button href="#" class="btn btn-primary btn-sm" onClick=saveVid()>Save</button>
       </div></div></div>`)
-
-
-
-
   }
+
+  function saveVid(){
+    vidResults.innerHTML = ``
+  }
+
 
   function resetVideo(){
     console.log("RESETTING");
