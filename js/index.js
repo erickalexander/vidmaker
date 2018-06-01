414bf1979e2463a70f68e2ea8e8b7f95aaf6a4bf
erickalexander <erickcamacho1@gmail.com>
2018-05-31 22:34:33 -0400

    created cards for video being fetched

--- js/index.js
+++ js/index.js
@@ -1,20 +1,41 @@
-let searchTerm = "dog"
-let fetchedVideos = fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyA33f7TdGW7pXSjQCPpiX-f4PNn-IGq73g&q=${searchTerm}&type=video`)
-  .then(resp => resp.json()).then(resp => {loadVideo(resp)})
+  document.getElementById('myForm').addEventListener('submit',fetchVideos)
 
-function loadVideo(resp){
-  vidResults.innerHTML += `<div class="well"><h3>${resp.etag}</h3> <button class="btn btn-success btn-sm"> + </button></div>`
+  console.log("LOADINGG");
+  let searchTerm = "cat"
+  let fetching = fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyA33f7TdGW7pXSjQCPpiX-f4PNn-IGq73g&q=${searchTerm}&type=video`)
+    .then(resp => resp.json())
+
+  function fetchVideos(e){
+    e.preventDefault()
+    console.log("FETCHING VIDS OF", searchTerm);
+
+    fetching.then(resp => {searchVideo(resp)})
+  }
+
+  function resetNow(){
+    vidResults.innerHTML = ``
+  }
+
+  function searchVideo(resp){
+    console.log('res',resp);
+    resp.items.map(v => vidResults.innerHTML += `<div class="col-sm-4"> <div class="card" style="width: 18rem;"><img class="card-img-top" src="${v.snippet.thumbnails.default.url}">  <div class="card-body">
+        <h5 class="card-title">${v.snippet.title}</h5>
+        <p class="card-text">${v.snippet.title}</p>
+        <a href="#" class="btn btn-primary">Save</a>
+      </div></div></div>`)
 
 
-}
-let reset = getElementById('reset')
 
-function resetVideo(){
-  console.log("RESETTING");
-  vidResults.innerHTML = ``
 
-}
+  }
 
-function searchVideo(resp){
+  function resetVideo(){
+    console.log("RESETTING");
+    vidResults.innerHTML = ``
 
-}
+  }
+
+  function loadVideo(res){
+    savedVideos.innerHTML += `<div><h3>${res.etag}</h3> <button class="btn btn-success "> + </button></div>`
+
+  }
