84495d31d691ec49661ac8262c51db273b5c2c52
erickalexander <erickcamacho1@gmail.com>
2018-06-07 20:57:28 -0400

    edited styles and temporarily saving videos, need to implement localstorage to save vids

--- js/index.js
+++ js/index.js
@@ -14,21 +14,53 @@
   }
 
 
-  function searchVideo(resp){
+  function searchVideo(resp,e){
     vidResults.innerHTML = ``
 
     console.log('res',resp);
-    resp.items.map(v => vidResults.innerHTML += `<div class="col-sm-5  vidResult"> <div class="card" style="width: 18rem;"> <iframe src="https://www.youtube.com/embed/${v.id.videoId}" width="320" height="180"></iframe>
+    resp.items.map((v,index) => vidResults.innerHTML += `<div id=${index} class="col-sm-5  vidResult"> <div class="card" style="width: 18rem;"> <iframe src="https://www.youtube.com/embed/${v.id.videoId}" width="320" height="180"></iframe>
    <div class="card-body">
 
         <h5 class="card-title">${v.snippet.title}</h5>
         <h6 class="card-text">${v.snippet.description}</h6>
-        <button type="button" href="#" class="btn btn-sm btn-success " onClick=saveVid()>Save</button>
+        <button type="button" href="#" class="btn btn-sm btn-success " onClick="saveVid(event)">Save</button>
       </div></div></div>`)
   }
 
-  function saveVid(){
-    vidResults.innerHTML = ``
+  function saveVid(e){
+
+    console.log(e,'inside save vid');
+
+    savedVideos.innerHTML = e.target.offsetParent.innerHTML
+
+    // let siteName = document.getElementById('sitename').value
+    // let siteUrl = document.getElementById('siteurl').value
+    //
+    // let video = {
+    //   name: siteName,
+    //   url: siteUrl
+    // }
+    //
+    // if(localStorage.getItem('bookmarks') === null){
+    //   let bookmarks = []
+    //   bookmarks.push(bookmark)
+    //   //set to localStorage
+    //
+    //   localStorage.setItem('bookmarks',JSON.stringify(bookmarks))
+    // }
+    // else{
+    //   //get bookmarks from localStorage
+    //   let bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+    //   //add bookmark to array
+    //   bookmarks.push(bookmark)
+    //   localStorage.setItem('bookmarks',JSON.stringify(bookmarks))
+    //
+    // }
+    //
+    // //Re-fetch Bookmarks
+    // fetchBookmarks()
+    //
+    // //prevent form from submitting
   }
 
 
